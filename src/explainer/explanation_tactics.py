0cafd6676af8311e615455919123df4a1999db55
nicoloddo <gnicoloddo@gmail.com>
2024-07-30 17:07:43 +0200

    Changed names to variables for clearer class understanding, added checks on explanation type and adjective instance name for tactic application; added SkipConditionStatement tactic

--- src/explainer/explanation_tactics.py
+++ src/explainer/explanation_tactics.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Any, List
 from src.explainer.common.utils import return_arguments
 
-from src.explainer.propositional_logic import Implies
+from src.explainer.propositional_logic import Implies, And
 
 from src.explainer.adjective import Adjective
 from src.explainer.framework import ArgumentationFramework
 
 from src.explainer.adjective import MaxRankAdjective, MinRankAdjective, NodesGroupPointerAdjective
 from src.explainer.adjective import QuantitativePointerAdjective
+from src.explainer.explanation import ConditionalExplanation
 
 class Tactic(ABC):
     """Abstract base class for all types of explanation tactics.
@@ -18,8 +19,16 @@ class Tactic(ABC):
 
     Tactics are called from adjectives during their explanation."""
 
-    def __init__(self, name):
+    def __init__(self, name, use_on_adjectives = []):
+        """
+        The tactic name will automatically be assigned by children classes.
+
+        The use_on_adjectives can be specified through the init of children classes
+        and should include names of adjectives from which the tactic can be executed.
+        If the use_on_adjectives is not specified, any adjective name will be allowed.
+        """
         self.name = name
+        self.use_on_adjectives = use_on_adjectives
         self.framework = None
         self.tactic_of_object = None
 
@@ -36,20 +45,56 @@ class Tactic(ABC):
         pass
 
     @property
-    @abstractmethod
-    # List of adjectives from which the tactic is performed.
+    # List of adjective types from which the tactic is performed.
+    # Do not override if you want to be able to use this tactic from
+    # any type of adjective (or return an empty array). 
+    # Override in any other occasion.
     # Examples below.
-    def acted_upon_from_adjectives(self) -> List:
-        pass
+    def exec_from_adjective_types(self) -> List:
+        return []
+    
+    @property
+    # List of explanation types onto which the tactic can be applied.
+    # Do not override if you want to be able to use this tactic on
+    # any type of explanation (or return an empty array). 
+    # Override in any other occasion.
+    # Examples below.
+    def allowed_on_explanation_types(self) -> List:
+        return []
 
     def validate_apply(self, calling_adjective):
-        for allowed_adjective in self.acted_upon_from_adjectives:
-            if isinstance(calling_adjective, allowed_adjective):
-                return True
-        return False  
-        #raise ValueError(f"{self.__class__.__name__} can only be APPLIED to adjectives among {self.acted_upon_from_adjectives}.")
+        allowed_adjective_type = False
+        allowed_adjective = False
+        allowed_explanation_type = False
 
-    def apply(self, calling_adjective, scope, *args) -> None:
+        if len(self.exec_from_adjective_types) == 0:
+            allowed_adjective_type = True
+        else:
+            for allowed_type in self.exec_from_adjective_types:
+                if isinstance(calling_adjective, allowed_type):
+                    allowed_adjective_type = True
+        
+        if len(self.use_on_adjectives) == 0:
+            allowed_adjective = True
+        else:
+            for allowed in self.use_on_adjectives:
+                if isinstance(calling_adjective.name, allowed):
+                    allowed_adjective = True
+
+        if len(self.allowed_on_explanation_types) == 0:
+            allowed_explanation_type = True
+        else:
+            for allowed_type in self.allowed_on_explanation_types:
+                if isinstance(calling_adjective.explanation, allowed_type):
+                    allowed_explanation_type = True
+        
+        return allowed_adjective_type and allowed_explanation_type and allowed_adjective
+    
+
+    def apply(self, calling_adjective, scope, *args) -> Any:
+        if all(arg is None for arg in args):
+            return return_arguments(*args)
+
         if not self.validate_apply(calling_adjective):
             return return_arguments(*args)
 
@@ -83,8 +128,8 @@ class SpecificTactic(Tactic):
     """Specific Tactics are allowed to start only from a selected adjective explanation."""
     @property
     @abstractmethod
-    def allowed_in_explanations_starting_from_adjectives(self) -> List:
-    # List of adjectives to which explanation the tactic will be applied from.
+    def allowed_in_expl_starting_from_adjective_types(self) -> List:
+    # List of adjective types from which explanation the tactic will be applied.
     # The tactic will be applied only if the explanation is starting from one of those.
     # Examples below.
         pass
@@ -97,15 +142,15 @@ class SpecificTactic(Tactic):
         self.validate_context()
 
     def validate_context(self):
-        for allowed_adjective in self.allowed_in_explanations_starting_from_adjectives:
+        for allowed_adjective in self.allowed_in_expl_starting_from_adjective_types:
             if isinstance(self.tactic_of_adjective, allowed_adjective):
                 return True        
-        raise ValueError(f"{self.__class__.__name__} can only be ATTACHED to adjectives among {self.allowed_in_explanations_starting_from_adjectives}.")
+        raise ValueError(f"{self.__class__.__name__} can only be ATTACHED to adjectives among {self.allowed_in_expl_starting_from_adjective_types}.")
     
 class GeneralTactic(Tactic):
     """General Tactics are allowed to start from any adjective explanation."""
     @property
-    def allowed_in_explanations_starting_from_adjectives(self) -> List:
+    def allowed_in_expl_starting_from_adjective_types(self) -> List:
     # List of adjectives to which explanation the tactic will be applied from.
         return [] # It will not be used as we override validate_context
     
@@ -131,11 +176,11 @@ class OnlyRelevantComparisons(SpecificTactic):
     eval_tactic = None
     
     @property
-    def allowed_in_explanations_starting_from_adjectives(self):
+    def allowed_in_expl_starting_from_adjective_types(self):
         return [MaxRankAdjective, MinRankAdjective]
     
     @property
-    def acted_upon_from_adjectives(self):
+    def exec_from_adjective_types(self):
         return [NodesGroupPointerAdjective]
     
     def __init__(self, *, mode: str):
@@ -190,7 +235,7 @@ class SkipQuantitativeExplanations(GeneralTactic):
     and pass directly to the explanation of the PointerAdjective instead."""
     
     @property
-    def acted_upon_from_adjectives(self):
+    def exec_from_adjective_types(self):
         return [QuantitativePointerAdjective]
 
     def __init__(self):
@@ -202,3 +247,30 @@ class SkipQuantitativeExplanations(GeneralTactic):
             return explanation.antecedent
         else:
             return None
+        
+class SkipConditionStatement(GeneralTactic):
+    """When explaining a QuantitativePointerAdjective, skip its statement
+    and pass directly to the explanation of the PointerAdjective instead."""
+
+    @property
+    def allowed_on_explanation_types(self) -> List:
+        return [ConditionalExplanation]
+
+    def __init__(self, *, use_on_adjectives: List = []):
+        """Leave use_on_adjectives blank if you want to use it on all adjectives."""
+        super().__init__(self.__class__.__name__, use_on_adjectives)
+
+    # apply    
+    def apply_on_explanation(self, explanation):
+        if isinstance(explanation, Implies):
+            antecedent = explanation.antecedent
+            antecedent.exprs = tuple(antecedent.exprs[1:]) # The first expression is the condition statement.
+            return explanation
+        elif isinstance(explanation, And): 
+            # we already have the antecedent, in occasions for example in which the consequent was cutted off
+            # e.g. quantitative explanation with skipping tactic
+            antecedent = explanation
+            antecedent.exprs = tuple(antecedent.exprs[1:]) # The first expression is the condition statement.
+            return explanation
+        else:
+            raise ValueError("Something went wrong with the SkipConditionStatement tactic: the explanation antecedent (ConditionalExplanation) should be an And expression but it is not.")
