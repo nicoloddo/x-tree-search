a485c39e5cc7d03045441548a70912e4fa227d33
nicoloddo <gnicoloddo@gmail.com>
2024-08-13 15:44:02 +0200

    Improved readability of minmax and added constraints possibility

--- algorithms/minmax.py
+++ algorithms/minmax.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 class MinMaxNode:
     """
     A wrapper for nodes in the game tree for use with the MinMax algorithm.
@@ -28,26 +30,26 @@ class MinMaxNode:
 
 class MinMax:
     """
-    Implements the MinMax algorithm to determine the optimal move for a player.
+    Implements the MinMax algorithm to determine the optimal child of a node.
 
     Attributes:
-        scoring_function (callable): A function that takes a game state and returns a numerical score.
+        scoring_function (callable): A function that takes a node state and returns a numerical score.
     """
     def __init__(self, game_tree, scoring_function):
         self.gmt = game_tree
         self.score = scoring_function
     
-    def run(self, node_id, depth=3):
-        self.gmt.expand_node(node_id, depth)
+    def run(self, node_id, *, depth=3, with_constraints: Dict = None):
+        self.gmt.expand_node(node_id, depth=depth, with_constraints=with_constraints) # constrain the who for example
         root_node = MinMaxNode(self.gmt.root)
-        best_value, best_move = self.minmax(root_node, True)
-        return best_move, best_value
+        best_child, best_value = self.minmax(root_node, True)
+        return best_child, best_value
     
     def minmax(self, node, is_maximizing):
         # If the node is a leaf, return its score
         if node.is_leaf:
-            node.score = self.score(node.node.state)
-            return node.score, node.id
+            node.score = self.score(node.node)
+            return node.id, node.score
         
         # Initialize best value
         if is_maximizing:
@@ -55,12 +57,12 @@ class MinMax:
         else:
             best_value = float('inf')
         
-        best_move = None
+        best_child = None
 
         for child in node.children:
             # If child does not have a score, recursively call minmax on the child
             if not child.has_score():
-                child_value, _ = self.minmax(child, not is_maximizing)
+                _, child_value = self.minmax(child, not is_maximizing)
                 child.score = child_value
             else:
                 child_value = child.score
@@ -69,12 +71,12 @@ class MinMax:
             if is_maximizing:
                 if child_value > best_value:
                     best_value = child_value
-                    best_move = child.id
+                    best_child = child.id
             else:
                 if child_value < best_value:
                     best_value = child_value
-                    best_move = child.id
+                    best_child = child.id
         
         # Assign the best value to the current node
         node.score = best_value
-        return best_value, best_move
+        return best_child, best_value
