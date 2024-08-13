e0c87a063184b5b8b4eeb57447182bf2a8b3faa9
nicoloddo <gnicoloddo@gmail.com>
2024-08-13 20:32:22 +0200

    Added more control on the 'free' label

--- games/tic_tac_toe.py
+++ games/tic_tac_toe.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from games.game import Game, GameModel, GameTree
 
+FREE_LABEL = ' '
 class TicTacToe(Game):
     
     def _game_model_definition(self) -> GameModel:
@@ -11,7 +12,7 @@ class TicTacToe(Game):
         agents_number=2, default_agent_features=['not starter', 'X'], additional_agent_features=[['starter'], ['O']], 
         agent_features_descriptions="2 players with feature 1 indicating who is starting, and feature 2 indicating their symbol.",
         game_name="tic-tac-toe")
-        gm.add_action_space("board", dimensions=[3, 3], default_labels=['free'], additional_labels=[['X', 'O']], dimensions_descriptions="3x3 board.")
+        gm.add_action_space("board", dimensions=[3, 3], default_labels=[FREE_LABEL], additional_labels=[['X', 'O']], dimensions_descriptions="3x3 board.")
 
         # Disable actions on the agent feature space.
         gm.disable_actions(on="agent")
@@ -22,21 +23,21 @@ class TicTacToe(Game):
             board = game.action_spaces["board"]
             # Check rows for winning condition
             for row in board:
-                if row[0] == row[1] == row[2] != 'free':
+                if row[0] == row[1] == row[2] != FREE_LABEL:
                     return True
 
             # Check columns for winning condition
             for col in range(3):
-                if board[0][col] == board[1][col] == board[2][col] != 'free':
+                if board[0][col] == board[1][col] == board[2][col] != FREE_LABEL:
                     return True
 
             # Check diagonals for winning condition
-            if board[0][0] == board[1][1] == board[2][2] != 'free':
+            if board[0][0] == board[1][1] == board[2][2] != FREE_LABEL:
                 return True
-            if board[0][2] == board[1][1] == board[2][0] != 'free':
+            if board[0][2] == board[1][1] == board[2][0] != FREE_LABEL:
                 return True
             
-            full_board = np.all(board != 'free')
+            full_board = np.all(board != FREE_LABEL)
             if full_board:
                 return True
 
@@ -47,7 +48,7 @@ class TicTacToe(Game):
 
         gm.action_is_violation_if(lambda who, where, what, game: not game.started and game.agents[who][0] != 'starter', rule_description="This is not the starting player and this is the first turn.")
         gm.action_is_violation_if(lambda who, where, what, game: game.started and who == game.actions[-1]['who'], rule_description="Players cannot play two times consecutively")
-        gm.action_is_violation_if(lambda who, where, what, game: where != 'free', "board", rule_description="The space needs to be free to put a sign on it.")
+        gm.action_is_violation_if(lambda who, where, what, game: where != FREE_LABEL, "board", rule_description="The space needs to be free to put a sign on it.")
         gm.action_is_violation_if(lambda who, where, what, game: game.agents[who][1] != what, rule_description="Agents can only put their own sign.")
 
         return gm
@@ -64,6 +65,3 @@ class TicTacToe(Game):
             raise ValueError("Player variable has to be 0 or 1")
 
         performed = self.model.action("board", player, coordinates, sign)
-
-        if performed:
-            print(self.model.action_spaces["board"])
