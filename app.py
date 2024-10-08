c7b5b22012b3bee0bb13b352d22d01ec80e59d72
nicoloddo <gnicoloddo@gmail.com>
2024-10-08 17:41:32 +0200

    Try to fix persistent game state across users

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from games.tic_tac_toe import TicTacToe, simple_depth_dependant_scoring_function
 from src.game.agents import GameAgent, User
 from algorithms.minimax import MiniMax
 from explainers.alphabeta_explainer import AlphaBetaExplainer
+import gradio as gr
 import asyncio
 
-explainer = AlphaBetaExplainer()
-opponent = GameAgent(agent_id=0, core=MiniMax(simple_depth_dependant_scoring_function, max_depth=6, use_alpha_beta=True))
-game = TicTacToe(players=[opponent, User(agent_id=1)], 
-                 explainer=explainer, 
-                 interface_mode='gradio', 
-                 interface_hyperlink_mode=True)
+def create_and_start_game():
+    explainer = AlphaBetaExplainer()
+    opponent = GameAgent(agent_id=0, core=MiniMax(simple_depth_dependant_scoring_function, max_depth=6, use_alpha_beta=True))
+    game = TicTacToe(players=[opponent, User(agent_id=1)], 
+                     explainer=explainer, 
+                     interface_mode='gradio', 
+                     interface_hyperlink_mode=True)
+    
+    # Run start_game asynchronously and get the interface
+    interface = asyncio.run(game.start_game())
+    return interface
 
-asyncio.run(game.start_game())
+if __name__ == "__main__":
+    iface = gr.Interface(fn=create_and_start_game, inputs=None, outputs="html")
+    iface.launch()
--- games/tic_tac_toe/interface.py
+++ games/tic_tac_toe/interface.py
@@ -227,14 +227,10 @@ class TicTacToeGradioInterface(GameInterface):
         self.explaining_agent = next((player for player in self.game.players.values() if not isinstance(player, User)), None)
         self.explainer_interface = ExplainerGradioInterface(game=self.game, explanation_depth=3, explaining_agent=self.explaining_agent, explain_in_hyperlink_mode=interface_hyperlink_mode)
 
-    def start(self, share_gradio=False):
+    def create_interface(self):
         """
-        Start the game interface.
-
-        Initializes the game state and launches the Gradio interface.
-        """
-        self.started = True
-        
+        Creates the game interface.
+        """        
         with gr.Blocks(css="""
             .grid-container {
                 grid-column-gap: 0px; 
@@ -331,7 +327,17 @@ class TicTacToeGradioInterface(GameInterface):
                 inputs=[],
                 outputs=all_available_outputs,
             )
-        
+
+        return demo        
+    
+    def start(self, share_gradio=False):
+        """
+        Start the game interface.
+
+        Initializes the game state and launches the Gradio interface.
+        """
+        self.started = True
+        demo = self.create_interface()
         demo.launch(share=share_gradio)
 
     def restart_game(self):
