858c0477737610679ecf9d604efbd7bfdbf2e142
Nicolò Loddo <gnicoloddo@gmail.com>
2024-10-30 18:12:22 +0000

    fixed wrong use of API

--- algorithms/test_minimax_opsp.py
+++ algorithms/test_minimax_opsp.py
@@ -7,6 +7,6 @@ tic_tac_toe = pyspiel.load_game("tic_tac_toe")
 state = tic_tac_toe.new_initial_state()
 algorithm = MiniMax(max_depth=30)
 
-game_score, action = algorithm.run(tic_tac_toe, state=state)
+game_score, action = algorithm.run(tic_tac_toe, state, 0)
 state.apply_action(action)
 print(state)
