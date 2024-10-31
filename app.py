eec9325a004fce385ddb16076b90ffc1a677a804
Nicolò Loddo <gnicoloddo@gmail.com>
2024-10-31 12:47:21 +0000

    app.py now supports tic tac toe OpenSpiel and does necessary imports depending on the game rather than globally

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from games.tic_tac_toe import TicTacToe, simple_depth_dependant_scoring_function as tic_tac_toe_scoring_function
-from games.breakthrough import Breakthrough, simple_depth_dependant_scoring_function as breakthrough_scoring_function
-from src.game.agents import AIAgent, User
-from algorithms.minimax import MiniMax
+from games.tic_tac_toe import simple_depth_dependant_scoring_function as tic_tac_toe_scoring_function
+from games.breakthrough import simple_depth_dependant_scoring_function as breakthrough_scoring_function
+
+from src.game.agents import AIAgent, AIAgentOpSp, User
 from games.tic_tac_toe.interface.gradio_interface import TicTacToeGradioInterface
 from games.breakthrough.interface.gradio_interface import BreakthroughGradioInterface
 from explainers.alphabeta_explainer import AlphaBetaExplainer
@@ -9,11 +9,23 @@ from explainers.alphabeta_explainer import AlphaBetaExplainer
 game = 'breakthrough'
 
 if game == 'tic_tac_toe':
+    from games.tic_tac_toe import TicTacToe
+    from algorithms.minimax import MiniMax
     ai_scoring_function = tic_tac_toe_scoring_function
     max_depth = 6
     game_class = TicTacToe
     interface_class = TicTacToeGradioInterface
+elif game == 'tic_tac_toe_opsp':
+    from games.tic_tac_toe import TicTacToeOpSp
+    from algorithms.minimax_openspiel_wrapper import MiniMax
+    AIAgent = AIAgentOpSp
+    ai_scoring_function = tic_tac_toe_scoring_function
+    max_depth = 30
+    game_class = TicTacToeOpSp
+    interface_class = TicTacToeGradioInterface
 elif game == 'breakthrough':
+    from games.breakthrough import Breakthrough
+    from algorithms.minimax import MiniMax
     ai_scoring_function = breakthrough_scoring_function
     max_depth = 4
     game_class = Breakthrough
@@ -21,7 +33,7 @@ elif game == 'breakthrough':
 
 use_ai_opponent = False
 
-opponent = AIAgent(agent_id=0, core=MiniMax(ai_scoring_function, max_depth=max_depth, use_alpha_beta=True))
+opponent = AIAgent(agent_id=0, core=MiniMax(ai_scoring_function, max_depth=max_depth))
 
 game = game_class(players=[opponent, User(agent_id=1)],
                 interface_mode='gradio', 
