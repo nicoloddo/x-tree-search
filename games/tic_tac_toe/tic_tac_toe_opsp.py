0990db69ce24195e0e10362020fcf6fad57aa6b9
Nicolò Loddo <gnicoloddo@gmail.com>
2024-10-31 16:49:14 +0000

    Moved game specific method before the general overrides methods comment separator

--- games/tic_tac_toe/tic_tac_toe_opsp.py
+++ games/tic_tac_toe/tic_tac_toe_opsp.py
@@ -73,6 +73,12 @@ class TicTacToeOpSp(TicTacToe):
         else:
             raise ValueError(f"The action {action_str} is not legal. Available actions are: {[self.state.action_to_string(player, action) for action in legal_actions]}")
     
+    def winner(self):
+        if np.all(self.action_spaces["board"] != FREE_LABEL):
+            return None
+        else:
+            return self.last_player
+            
     """General overrides for OpSp game classes:"""
     @property
     def started(self):
@@ -94,9 +100,3 @@ class TicTacToeOpSp(TicTacToe):
     
     def get_current_player(self):
         return self.players[self.state.current_player()]
-    
-    def winner(self):
-        if np.all(self.action_spaces["board"] != FREE_LABEL):
-            return None
-        else:
-            return self.last_player
