a24fd845116905bd273c19e86b4e6631d810da4b
Nicolò Loddo <gnicoloddo@gmail.com>
2024-11-01 13:36:25 +0000

    Fixed the minimax explainer after recent API changes in condition inits

--- explainers/minimax_explainer.py
+++ explainers/minimax_explainer.py
@@ -35,7 +35,7 @@ class MiniMaxExplainer:
                         definition = "node.readable_score",
 
                         explanation = ConditionalExplanation(
-                            condition = If("leaf"),
+                            condition = If("possession", "leaf"),
                             explanation_if_true = Assumption("Leaf nodes have scores from the evaluation function"),
                             explanation_if_false = CompositeExplanation(
                                 Assumption("Internal nodes have scores from children"),
@@ -51,7 +51,7 @@ class MiniMaxExplainer:
                         definition = "node.score_child",
 
                         explanation = ConditionalExplanation(
-                            condition = If("opponent player turn"),
+                            condition = If("possession", "opponent player turn"),
                             explanation_if_true = CompositeExplanation(
                                 Assumption("We assume the opponent will do their best move."),
                                 Possession("backpropagating child", "worst")),
@@ -66,9 +66,9 @@ class MiniMaxExplainer:
                         definition = "node.parent.children",
                         excluding = "node"),
 
-                    MaxRankAdjective("best", "better than", "siblings"),
+                    MaxRankAdjective("best", ["better than"], "siblings"),
 
-                    MinRankAdjective("worst", "better than", "siblings"),
+                    MinRankAdjective("worst", ["better than"], "siblings"),
                 ],
 
                 main_explanation_adjective = "best",
@@ -95,7 +95,7 @@ class MiniMaxExplainer:
                         definition = "node.score",
 
                         explanation = ConditionalExplanation(
-                            condition = If("final move"),
+                            condition = If("possession", "final move"),
                             explanation_if_true = Assumption("final moves are evaluated only looking at the final position", necessary=True),
                             explanation_if_false = CompositeExplanation(
                                 Possession("as next possible move", "as score"))
@@ -110,7 +110,7 @@ class MiniMaxExplainer:
                         definition = "node.score_child",
 
                         explanation = ConditionalExplanation(
-                            condition = If("opponent player turn"),
+                            condition = If("possession", "opponent player turn"),
                             explanation_if_true = CompositeExplanation(
                                 Assumption("we assume the opponent will do their best move"),
                                 Possession("as next possible move", "the best the opponent can do")),
@@ -125,10 +125,11 @@ class MiniMaxExplainer:
                         definition = "node.parent.children",
                         excluding = "node"),
 
-                    MaxRankAdjective("the best", "better than", "as possible alternative moves",
-                                    tactics = [OnlyRelevantComparisons(mode = "top_3")]),
+                    MaxRankAdjective("the best", ["better than"], "as possible alternative moves",
+                                    #tactics = [OnlyRelevantComparisons(mode = "top_3")] this tactic needs fixing
+                                    ),
 
-                    MinRankAdjective("the best the opponent can do", "better than", "as possible alternative moves"),
+                    MinRankAdjective("the best the opponent can do", ["better than"], "as possible alternative moves"),
                 ],
                 
                 main_explanation_adjective = "the best",
