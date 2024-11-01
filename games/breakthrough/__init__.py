669d0a345daf6173a32387a8bf246a2728da8bbf
Nicolò Loddo <gnicoloddo@gmail.com>
2024-11-01 13:36:39 +0000

    Allow breakthrough opsp import

--- games/breakthrough/__init__.py
+++ games/breakthrough/__init__.py
@@ -1,2 +1,7 @@
 from .breakthrough import Breakthrough
+try:
+    from .breakthrough_opsp import BreakthroughOpSp
+except ImportError:
+    pass
+
 from .scoring import simple_depth_dependant_scoring_function
