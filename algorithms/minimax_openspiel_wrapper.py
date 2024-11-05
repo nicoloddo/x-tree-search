f36397b3b8dc0a6db427cbd11e5fd0f146a8766c
Nicolò Loddo <gnicoloddo@gmail.com>
2024-11-05 09:55:13 +0000

    Fixed depth not being considered because at max depth we were always with depth 0 given the OpenSpiel way of using the depth

--- algorithms/minimax_openspiel_wrapper.py
+++ algorithms/minimax_openspiel_wrapper.py
@@ -2,10 +2,11 @@ from functools import wraps
 from typing import List, Tuple, Any, Dict
 
 class StateActionTracker:
-    def __init__(self, start_with_maximizing):
+    def __init__(self, start_with_maximizing, max_depth):
         self.root = None
         self.nodes = {}
         self.start_with_maximizing = start_with_maximizing
+        self.max_depth = max_depth
 
     def set_root(self, node):
         self.root = node
@@ -167,7 +168,7 @@ def track_state_actions(tracker: StateActionTracker):
             
             tracker.nodes[node.id] = node
             
-            node.depth = depth
+            node.depth = tracker.max_depth - depth # In OpenSpiel we start at the max depth and subtract from it
             value, best_action = func(node, depth, alpha, beta, value_function, maximizing_player_id)
             node.score = value
             
@@ -175,9 +176,7 @@ def track_state_actions(tracker: StateActionTracker):
             for action in node.legal_actions()[len(node.children):]:
                 missing_child = node.clone()
                 missing_child.apply_action(action)
-                missing_child.depth = depth + 1
-                missing_child.alpha = alpha
-                missing_child.beta = beta
+                missing_child.depth = node.depth + 1
                 tracker.nodes[node.id + '_' + str(action)] = missing_child"""
             
             return value, best_action
@@ -214,7 +213,7 @@ class MiniMax:
         :param max_depth: The maximum depth to search to.
         """
         try:
-            self.tracker = StateActionTracker(self.start_with_maximizing)
+            self.tracker = StateActionTracker(self.start_with_maximizing, self.max_depth)
 
             # If already wrapped, get the original function
             if hasattr(minimax._alpha_beta, '_original_func'):
