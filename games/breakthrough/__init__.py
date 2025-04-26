d02bda36860d322a04984f8be37a803c0a5ff1ec
Nicolò Loddo <nicolo@getmagpie.ai>
2025-04-26 20:33:26 +0200

    formatted

--- games/breakthrough/__init__.py
+++ games/breakthrough/__init__.py
@@ -1,7 +1,11 @@
 from .breakthrough import Breakthrough
+
 try:
     from .breakthrough_opsp import BreakthroughOpSp
 except ImportError:
     pass
 
-from .scoring import simple_depth_dependant_scoring_function, simple_depth_dependant_scoring_function_opsp
+from .scoring import (
+    simple_depth_dependant_scoring_function,
+    simple_depth_dependant_scoring_function_opsp,
+)
